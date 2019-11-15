d788bd43e5304d5457c24d9d8ac67d7eade2b355
Fernando Alamillo <fernando.alamillo@creative-technologies.co.uk>
2019-11-15 15:12:11 +0000

    Updates

---
+++ precache-manifest.59a33ffc7a654c3ec014abe83d53b0c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eed33731ace0e62180e379ee38e8a8d2",
+    "url": "/hotjar-demo/index.html"
+  },
+  {
+    "revision": "de2007e50c2008c9c612",
+    "url": "/hotjar-demo/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "4150beda53179b1e0c7f",
+    "url": "/hotjar-demo/static/js/2.78f477bd.chunk.js"
+  },
+  {
+    "revision": "de2007e50c2008c9c612",
+    "url": "/hotjar-demo/static/js/main.b2301682.chunk.js"
+  },
+  {
+    "revision": "515d610db37d222fa180",
+    "url": "/hotjar-demo/static/js/runtime-main.da3ad002.js"
+  }
+]);
