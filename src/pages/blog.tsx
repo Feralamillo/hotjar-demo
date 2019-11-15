48ea3a48879f72540c3b28735af9e9ed97ad008f
Fernando Alamillo <fernando.alamillo@creative-technologies.co.uk>
2019-11-15 16:56:52 +0000

    feat(blog) including page

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,10 +1,455 @@
-import React from "react";
+import React from 'react';
 
 export const Blog: React.FunctionComponent = () => {
   return (
     <React.Fragment>
-      <div>BlogPage</div>
-      <div>Working</div>
+      <header className="header-section clearfix">
+        <div className="container-fluid">
+          <a href="index.html" className="site-logo">
+            <img src="img/logo.png" alt="" />
+          </a>
+          <div className="responsive-bar">
+            <i className="fa fa-bars"></i>
+          </div>
+          <a href="/" className="user">
+            <i className="fa fa-user"></i>
+          </a>
+          <a href="/" className="site-btn">
+            Sign Up Free
+          </a>
+          <nav className="main-menu">
+            <ul className="menu-list">
+              <li>
+                <a href="/">Solution</a>
+              </li>
+              <li>
+                <a href="/">Features</a>
+              </li>
+              <li>
+                <a href="/">News</a>
+              </li>
+              <li>
+                <a href="/">About</a>
+              </li>
+              <li>
+                <a href="/">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+      <section className="page-info-section">
+        <div className="container">
+          <h2>News</h2>
+          <div className="site-beradcamb">
+            <a href="/">Home</a>
+            <span>
+              <i className="fa fa-angle-right"></i> News
+            </span>
+          </div>
+        </div>
+      </section>
+
+      <section className="blog-page spad">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="blog-item bi-feature">
+                    <figure className="blog-thumb">
+                      <img src="img/blog/2.jpg" alt="" />
+                    </figure>
+                    <div className="blog-text">
+                      <div className="post-date">22 jan 2018</div>
+                      <h4 className="blog-title">
+                        <a href="single-blog.html">This Week in Bitcoin: Up, Down and Sideways</a>
+                      </h4>
+                      <div className="post-meta">
+                        <a href="/">
+                          <span>by</span> Admin
+                        </a>
+                        <a href="/">
+                          <i className="fa fa-heart-o"></i> 234 Likes
+                        </a>
+                        <a href="/">
+                          <i className="fa fa-comments-o"></i> 08 comments
+                        </a>
+                      </div>
+                      <p>
+                        Bitcoin is one of the most important inventions in all of human history. For the first time ever, anyone can send or receive
+                        any amount of money with anyone else, anywhere on the planet, conveniently and without restriction. It’s the dawn of a better,
+                        more free world.
+                      </p>
+                      <a href="/" className="readmore">
+                        Readmore <i className="fa fa-angle-double-right"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="blog-item">
+                    <figure className="blog-thumb">
+                      <img src="img/blog/1.jpg" alt="" />
+                    </figure>
+                    <div className="blog-text">
+                      <div className="post-date">22 dec 2018</div>
+                      <h4 className="blog-title">
+                        <a href="single-blog.html">Blockchain Rolls Out Trading Feature for 22 States in the U.S</a>
+                      </h4>
+                      <div className="post-meta">
+                        <a href="/">
+                          <span>by</span> Admin
+                        </a>
+                        <a href="/">
+                          <i className="fa fa-heart-o"></i> 234 Likes
+                        </a>
+                        <a href="/">
+                          <i className="fa fa-comments-o"></i> 08 comments
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="blog-item">
+                    <figure className="blog-thumb">
+                      <img src="img/blog/3.jpg" alt="" />
+                    </figure>
+                    <div className="blog-text">
+                      <div className="post-date">28 aug 2018</div>
+                      <h4 className="blog-title">
+                        <a href="single-blog.html">This Week in Bitcoin: Up, Down and Sideways</a>
+                      </h4>
+                      <div className="post-meta">
+                        <a href="/">
+                          <span>by</span> Admin
+                        </a>
+                        <a href="/">
+                          <i className="fa fa-heart-o"></i> 234 Likes
+                        </a>
+                        <a href="/">
+                          <i className="fa fa-comments-o"></i> 08 comments
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="blog-item">
+                    <figure className="blog-thumb">
+                      <img src="img/blog/4.jpg" alt="" />
+                    </figure>
+                    <div className="blog-text">
+                      <div className="post-date">22 jan 2018</div>
+                      <h4 className="blog-title">
+                        <a href="single-blog.html">Indians Expect Clarity on Bitcoin Taxes within Days</a>
+                      </h4>
+                      <div className="post-meta">
+                        <a href="/">
+                          <span>by</span> Admin
+                        </a>
+                        <a href="/">
+                          <i className="fa fa-heart-o"></i> 234 Likes
+                        </a>
+                        <a href="/">
+                          <i className="fa fa-comments-o"></i> 08 comments
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="blog-item">
+                    <figure className="blog-thumb">
+                      <img src="img/blog/5.jpg" alt="" />
+                    </figure>
+                    <div className="blog-text">
+                      <div className="post-date">22 dec 2018</div>
+                      <h4 className="blog-title">
+                        <a href="single-blog.html">Thailand Taking Steps to Regulate ICOs</a>
+                      </h4>
+                      <div className="post-meta">
+                        <a href="/">
+                          <span>by</span> Admin
+                        </a>
+                        <a href="/">
+                          <i className="fa fa-heart-o"></i> 234 Likes
+                        </a>
+                        <a href="/">
+                          <i className="fa fa-comments-o"></i> 08 comments
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="blog-item">
+                    <figure className="blog-thumb">
+                      <img src="img/blog/6.jpg" alt="" />
+                    </figure>
+                    <div className="blog-text">
+                      <div className="post-date">22 jan 2018</div>
+                      <h4 className="blog-title">
+                        <a href="single-blog.html">Discover Card’s 44 Million Customers Denied Crypto</a>
+                      </h4>
+                      <div className="post-meta">
+                        <a href="/">
+                          <span>by</span> Admin
+                        </a>
+                        <a href="/">
+                          <i className="fa fa-heart-o"></i> 234 Likes
+                        </a>
+                        <a href="/">
+                          <i className="fa fa-comments-o"></i> 08 comments
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="blog-item">
+                    <figure className="blog-thumb">
+                      <img src="img/blog/7.jpg" alt="" />
+                    </figure>
+                    <div className="blog-text">
+                      <div className="post-date">22 dec 2018</div>
+                      <h4 className="blog-title">
+                        <a href="single-blog.html">Wall Street Price Manipulation? Go Long</a>
+                      </h4>
+                      <div className="post-meta">
+                        <a href="/">
+                          <span>by</span> Admin
+                        </a>
+                        <a href="/">
+                          <i className="fa fa-heart-o"></i> 234 Likes
+                        </a>
+                        <a href="/">
+                          <i className="fa fa-comments-o"></i> 08 comments
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <button className="post-loadmore site-btn sb-gradients sbg-line mt-5">LOAD MORE</button>
+            </div>
+            <div className="col-lg-4 col-md-6 sideber pt-5 pt-lg-0">
+              <div className="widget-area">
+                <h4 className="widget-title">News Letter</h4>
+                <p>Receive our newsletter to stay on top of the latest posts.</p>
+                <form className="widget-subscribe-from">
+                  <input type="text" placeholder="Enter your email" />
+                  <button className="site-btn sb-full-- sb-gradients">Subscribe</button>
+                </form>
+              </div>
+              <div className="widget-area">
+                <div className="widget">
+                  <h4 className="widget-title">Categories</h4>
+                  <ul>
+                    <li>
+                      <a href="#">Prediction markets</a>
+                    </li>
+                    <li>
+                      <a href="#">Storage</a>
+                    </li>
+                    <li>
+                      <a href="#">Token exchange</a>
+                    </li>
+                    <li>
+                      <a href="#">Computation</a>
+                    </li>
+                    <li>
+                      <a href="#">Identity</a>
+                    </li>
+                    <li>
+                      <a href="#">ICOs</a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="widget">
+                  <h4 className="widget-title">Popular Posts</h4>
+                  <ul className="popular-posts">
+                    <li>
+                      <span>22 dec 2018</span>
+                      <h5>
+                        <a href="/">Lightning and Mainnet: A Look at the Protocol’s Hype, Trials, and Error</a>
+                      </h5>
+                    </li>
+                    <li>
+                      <span>22 dec 2018</span>
+                      <h5>
+                        <a href="/">This Week in Bitcoin: Japan Gets Goxxed and Iota Gets Into a Tangle</a>
+                      </h5>
+                    </li>
+                    <li>
+                      <span>22 dec 2018</span>
+                      <h5>
+                        <a href="/">Bitcoin Futures Report Shows Bullish Sentiment Is In the Air</a>
+                      </h5>
+                    </li>
+                    <li>
+                      <span>22 dec 2018</span>
+                      <h5>
+                        <a href="/">Why Venezuela’s New National Cryptocurrency El Petro Will Fail</a>
+                      </h5>
+                    </li>
+                  </ul>
+                </div>
+                <div className="widget">
+                  <h4 className="widget-title">Recent Tweets</h4>
+                  <ul className="twitter-widget">
+                    <li>
+                      <h5>Why Does The Bitcoin Price Move So Much?</h5>
+                      <a href="https://t.co/MSQVkamNwa">https://t.co/MSQVkamNwa</a>
+                      <span>4 days ago</span>
+                    </li>
+                    <li>
+                      <h5>How Can I Trade Bitcoin without an Exchange?</h5>
+                      <a href="https://t.co/MSQVkamNwa">https://t.co/MSQVkamNwa</a>
+                      <span>4 days ago</span>
+                    </li>
+                  </ul>
+                </div>
+                <div className="widget">
+                  <h4 className="widget-title">Follow Us</h4>
+                  <div className="social">
+                    <a href="/" className="facebook">
+                      <i className="fa fa-facebook"></i>
+                    </a>
+                    <a href="/" className="google">
+                      <i className="fa fa-google-plus"></i>
+                    </a>
+                    <a href="/" className="instagram">
+                      <i className="fa fa-instagram"></i>
+                    </a>
+                    <a href="/" className="twitter">
+                      <i className="fa fa-twitter"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="newsletter-section gradient-bg">
+        <div className="container text-white">
+          <div className="row">
+            <div className="col-lg-7 newsletter-text">
+              <h2>Subscribe to our Newsletter</h2>
+              <p>Sign up for our weekly industry updates, insider perspectives and in-depth market analysis.</p>
+            </div>
+            <div className="col-lg-5 col-md-8 offset-lg-0 offset-md-2">
+              <form className="newsletter-form">
+                <input type="text" placeholder="Enter your email" />
+                <button>Get Started</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer className="footer-section">
+        <div className="container">
+          <div className="row spad">
+            <div className="col-md-6 col-lg-3 footer-widget">
+              <img src="img/logo.png" className="mb-4" alt="" />
+              <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia dese mollit anim id est laborum.</p>
+            </div>
+            <div className="col-md-6 col-lg-2 offset-lg-1 footer-widget">
+              <h5 className="widget-title">Resources</h5>
+              <ul>
+                <li>
+                  <a href="#">How to Buy Coin</a>
+                </li>
+                <li>
+                  <a href="#">Coin Overview</a>
+                </li>
+                <li>
+                  <a href="#">Blog News</a>
+                </li>
+                <li>
+                  <a href="#">How to Sell Coin</a>
+                </li>
+                <li>
+                  <a href="#">Purchase Theme</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-6 col-lg-2 offset-lg-1 footer-widget">
+              <h5 className="widget-title">Quick Links</h5>
+              <ul>
+                <li>
+                  <a href="#">Network Stats</a>
+                </li>
+                <li>
+                  <a href="#">Block Explorers</a>
+                </li>
+                <li>
+                  <a href="#">Governance</a>
+                </li>
+                <li>
+                  <a href="#">Exchange Markets</a>
+                </li>
+                <li>
+                  <a href="#">Get Theme</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-6 col-lg-3 footer-widget pl-lg-5 pl-3">
+              <h5 className="widget-title">Follow Us</h5>
+              <div className="social">
+                <a href="/" className="facebook">
+                  <i className="fa fa-facebook"></i>
+                </a>
+                <a href="/" className="google">
+                  <i className="fa fa-google-plus"></i>
+                </a>
+                <a href="/" className="instagram">
+                  <i className="fa fa-instagram"></i>
+                </a>
+                <a href="/" className="twitter">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="footer-bottom">
+            <div className="row">
+              <div className="col-lg-4 store-links text-center text-lg-left pb-3 pb-lg-0">
+                <a href="/">
+                  <img src="img/appstore.png" alt="" className="mr-2" />
+                </a>
+                <a href="/">
+                  <img src="img/playstore.png" alt="" />
+                </a>
+              </div>
+              <div className="col-lg-8 text-center text-lg-right">
+                <ul className="footer-nav">
+                  <li>
+                    <a href="/">DPA</a>
+                  </li>
+                  <li>
+                    <a href="/">Terms of Use</a>
+                  </li>
+                  <li>
+                    <a href="/">Privacy Policy </a>
+                  </li>
+                  <li>
+                    <a href="/">support@company.com</a>
+                  </li>
+                  <li>
+                    <a href="/">(123) 456-7890</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </React.Fragment>
   );
 };
