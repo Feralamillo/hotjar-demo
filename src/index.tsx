9c60de20259f067cefd2801d100bdc11812e2334
Fernando Alamillo <fernando.alamillo@creative-technologies.co.uk>
2019-11-18 11:57:42 +0000

    GTM

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TagManager from 'react-gtm-module';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import TagManager from 'react-gtm-module';
-
 const tagManagerArgs = {
-  gtmId: 'GTM-N535FZF',
+  gtmId: process.env.GTM || '',
 };
 
 TagManager.initialize(tagManagerArgs);
